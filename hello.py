129b9e67b643f474c3d61554a15679f23c2fed79
Beatrice Ionascu <beatrice.ionascu@nyu.edu>
2016-08-05 15:26:19 -0400

    created migration repository and automatic migration script, and upgraded database

--- hello.py
+++ hello.py
@@ -5,13 +5,14 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 # from flask import abort
 from flask_script import Manager
 # from flask import current_app
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
-from flask.ext.wtf import Form
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.script import Shell
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Shell
+from flask_migrate import Migrate, MigrateCommand
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -19,11 +20,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mySecretKeyForFlasky'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Role(db.Model):
@@ -53,7 +56,9 @@ class NameForm(Form):
 
 def make_shell_context():
 	return dict(app = app, db = db, User = User, Role = Role)
-manager.add_command("shell", Shell(make_context = make_shell_context))
+
+manager.add_command('shell', Shell(make_context = make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 @app.errorhandler(404)
 def page_not_found(e):
