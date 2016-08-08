23edd00785c39b545af420c0ec5eea5295c09c24
Beatrice Ionascu <beatrice.ionascu@nyu.edu>
2016-08-08 16:27:25 -0400

    converted to large application structure

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	SQLALCHEMY_TRACK_MODIFICATIONS = True
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	BLOGGING_APP_MAIL_SUBJECT_PREFIX = '[blogging_app] '
+	BLOGGING_APP_MAIL_SENDER = 'Blogging App Admin <{}>'.format(os.environ.get('MAIL_USERNAME'))
+	BLOGGING_APP_ADMIN = os.environ.get('BLOGGING_APP_ADMIN')
+
+	@staticmethod
+	def init_app(app):
+		pass
+
+
+class DevelopmentConfig(Config):
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+	'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+	TESTING = True
+	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+	'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+	'development': DevelopmentConfig,
+	'testing': TestingConfig,
+	'production': ProductionConfig,
+
+	'default': DevelopmentConfig
+}
